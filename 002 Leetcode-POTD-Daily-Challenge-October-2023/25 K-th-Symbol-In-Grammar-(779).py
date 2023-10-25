f0eb71810d531b35530baa3f3a8e7300960bc7d8
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2023-10-25 23:18:10 +0530

    October-2023

---
+++ 002 Leetcode-POTD-Daily-Challenge-October-2023/25 K-th-Symbol-In-Grammar-(779).py
@@ -0,0 +1,14 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        # the pattern repeats itself but inversing
+
+        def find(level, k, inverted):
+            if level == 1:
+                return 0 if not inverted else 1
+            level_elements = 2 ** (level - 1)
+            previous_level_elements = level_elements // 2
+            if k > previous_level_elements:
+                return find(level - 1, k - previous_level_elements, not inverted)
+            return find(level - 1, k, inverted)
+
+        return find(n, k, False)
