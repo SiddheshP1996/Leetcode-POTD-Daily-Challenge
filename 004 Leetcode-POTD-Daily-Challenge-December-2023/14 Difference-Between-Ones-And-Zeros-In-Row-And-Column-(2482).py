49fecb24285236d2b011431c88fc9f59a0a87b11
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2023-12-14 22:59:30 +0530

    December-2023

--- 004 Leetcode-POTD-Daily-Challenge-December-2023/14 Difference-Between-Ones-And-Zeros-In-Row-And-Column-(2482).py
+++ 004 Leetcode-POTD-Daily-Challenge-December-2023/14 Difference-Between-Ones-And-Zeros-In-Row-And-Column-(2482).py
@@ -2,17 +2,17 @@ class Solution:
     def onesMinusZeros(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
         m = len(grid[0])
-        onesKeRows = [0] * n
-        onesKeColumns = [0] * m
+        onesInRows = [0] * n
+        onesInColumns = [0] * m
         result = [[0 for _ in range(m)] for _ in range(n)]
         for i in range(n):
             for j in range(m):
-                onesKeRows[i] += grid[i][j]
-                onesKeColumns[j] += grid[i][j]
+                onesInRows[i] += grid[i][j]
+                onesInColumns[j] += grid[i][j]
 
         for i in range(n):
             for j in range(m):
-                zerosKeRows = n - onesKeRows[i]
-                zerosKeColumns = m - onesKeColumns[j]
-                result[i][j] = onesKeRows[i] + onesKeColumns[j] - zerosKeRows - zerosKeColumns
+                zerosInRows = n - onesInRows[i]
+                zerosInColumns = m - onesInColumns[j]
+                result[i][j] = onesInRows[i] + onesInColumns[j] - zerosInRows - zerosInColumns
         return result
