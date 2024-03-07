f1fdbf08f1bf9dde63377092162d45c955e3f035
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-03-07 22:58:10 +0530

    Year-2024

---
+++ 2024/003 Leetcode-POTD-Daily-Challenge-March-2024/07 Middle-Of-The-Linked-List-(876).py
@@ -0,0 +1,20 @@
+"""
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+"""
+
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head.next
+
+        while fast is not None:
+            slow = slow.next
+            fast = fast.next
+            if fast is not None:
+                fast = fast.next
+        
+        return slow
