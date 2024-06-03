6b19c0e02108285f871931e327bd9ca04a6eee8f
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-06-03 20:49:54 +0530

    Year-2024

---
+++ 2024/006 Leetcode-POTD-Daily-Challenge-June-2024/03 Append-Characters-To-String-To-Make-Subsequence-(2486).py
@@ -0,0 +1,11 @@
+class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        n = len(s)
+        m = len(t)
+        j = 0
+        for i in range(n):
+            if j == m:
+                return 0
+            if s[i] == t[j]:
+                j += 1
+        return m - j
