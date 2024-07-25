682dbd565cbc192b7568c3a0c3ea15d29a18e1e4
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-07-25 17:40:01 +0530

    Year-2024

---
+++ 2024/07 Leetcode-POTD-Daily-Challenge-July-2024/25 Sort-An-Array-(912).py
@@ -0,0 +1,33 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def mergeSort(arr):
+            if len(arr) > 1:
+                mid = len(arr) // 2
+                leftHalf = arr[:mid]
+                rightHalf = arr[mid:]
+
+                mergeSort(leftHalf)
+                mergeSort(rightHalf)
+
+                i = j = k = 0
+                while i < len(leftHalf) and j < len(rightHalf):
+                    if leftHalf[i] < rightHalf[j]:
+                        arr[k] = leftHalf[i]
+                        i += 1
+                    else:
+                        arr[k] = rightHalf[j]
+                        j += 1
+                    k += 1
+
+                while i < len(leftHalf):
+                    arr[k] = leftHalf[i]
+                    i += 1
+                    k += 1
+
+                while j < len(rightHalf):
+                    arr[k] = rightHalf[j]
+                    j += 1
+                    k += 1
+
+        mergeSort(nums)
+        return nums
