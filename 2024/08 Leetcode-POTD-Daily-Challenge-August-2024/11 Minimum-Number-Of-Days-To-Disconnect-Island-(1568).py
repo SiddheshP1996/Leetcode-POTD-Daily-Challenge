2541237abde3cda0e90e7b5211687cad7700cdc6
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-08-11 19:01:38 +0530

    Year-2024

---
+++ 2024/08 Leetcode-POTD-Daily-Challenge-August-2024/11 Minimum-Number-Of-Days-To-Disconnect-Island-(1568).py
@@ -0,0 +1,35 @@
+class Solution:
+    def minDays(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+
+        def countIslands():
+            visited = set()
+            count = 0
+            for i in range(rows):
+                for j in range(cols):
+                    if grid[i][j] == 1 and (i, j) not in visited:
+                        exploreIsland(i, j, visited)
+                        count += 1
+            return count
+
+        def exploreIsland(i, j, visited):
+            if (i < 0 or i >= rows or j < 0 \
+                or j >= cols or grid[i][j] == 0 \
+                    or (i, j) in visited):
+                return
+            visited.add((i, j))
+            for di, dj in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                exploreIsland(i + di, j + dj, visited)
+
+        if countIslands() != 1:
+            return 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 1:
+                    grid[i][j] = 0
+                    if countIslands() != 1:
+                        return 1
+                    grid[i][j] = 1
+
+        return 2
