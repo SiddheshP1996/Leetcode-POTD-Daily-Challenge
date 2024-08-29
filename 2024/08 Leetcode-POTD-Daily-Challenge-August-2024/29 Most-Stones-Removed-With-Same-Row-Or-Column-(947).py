74c9c57f027e027436fc098444b66f6a52f26d59
Siddhesh Panajkar <67057053+SiddheshP1996@users.noreply.github.com>
2024-08-29 22:31:02 +0530

    Year-2024

---
+++ 2024/08 Leetcode-POTD-Daily-Challenge-August-2024/29 Most-Stones-Removed-With-Same-Row-Or-Column-(947).py
@@ -0,0 +1,29 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        n = len(stones)
+        visited = [False] * n
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+        result = 0
+        for i, point in enumerate(stones):
+            rows[point[0]].append(i)
+            cols[point[1]].append(i)
+        
+        def dfs(node):
+            visited[node] = True
+            count = 1
+            for x in rows[stones[node][0]]:
+                if visited[x] == False:
+                    count += dfs(x)
+                    
+            for x in cols[stones[node][1]]:
+                if visited[x] == False:
+                    count += dfs(x)
+                    
+            return count
+            
+        for i in range(n):
+            if visited[i] == False:
+                size = dfs(i)
+                result += size-1
+        return result
